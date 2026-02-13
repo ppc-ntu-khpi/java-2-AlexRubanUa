7e55a579fa20b61b504c55448ecd1e849e9ed4a5
AlexRubanUa <urbanvinegar@gmail.com>
2026-02-13 14:22:27 +0200

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,25 @@
-//package domain;
-
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static boolean Calculate(String word){
+        if(word == null || word.isEmpty()){
+            return false;
+        }
+        int left = 0;
+        int right = word.length() - 1;
+        while(left < right){
+            if(word.charAt(left) != word.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static String Reverse(String word){
+        if(word == null || word.isEmpty()){
+            return "error";
+        }
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        return reversedWord;
     }
 }
+
